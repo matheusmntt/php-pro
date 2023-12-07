3ff720e7f9ad71df98e307518519a1ccd3048ad4
matheusmntt <matheus.monteiro7@outlook.com>
2023-12-07 02:20:23 -0300

    feat: added a template system

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,10 +2,14 @@
 
 namespace app\controllers;
 
+use League\Plates\Engine;
+
 class UserController
 {
   public function edit($params)
   {
-    dd($params);
+    $templates = new Engine('../app/views');
+
+    echo $templates->render('user', ['name' => 'Matheus']);
   }
 }
--- app/router/Routes.php
+++ app/router/Routes.php
@@ -9,7 +9,7 @@ class Routes
     return [
       'get' => [
         '/' => 'HomeController@index',
-        '/user/edit/[0-9]+' => 'UserController@edit',
+        '/user/[0-9]+' => 'UserController@edit',
         '/product/[a-z]+/category/[a-z]+' => 'ProductController@show',
         '/register' => 'RegisterController@index',
       ],
